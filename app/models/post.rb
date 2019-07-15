5173898c0554063ba83bb65247b6c0271b584069
sino <smk16sound@gmail.com>
2019-07-15 22:49:11 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  belongs_to :user, optional: true
+  # has_many_attached :images
+  # has_many :likes, dependent: :destroy
+  mount_uploader :image, ImageUploader
+
+  # belongs_to :guidebook, optional: true
+  # acts_as_list
+
+  # def like_user(user_id)
+  #  likes.find_by(user_id: user_id)
+  # end
 end
